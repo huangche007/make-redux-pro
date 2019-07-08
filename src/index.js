051519f7876f32a2c67c9b7cb54ebcceb080ac32
huangche <hc386271623@163.com>
2019-07-08 18:59:00 +0800

    [add feature]初始化项目结构

--- src/index.js
+++ src/index.js
@@ -1,50 +1,22 @@
-const appState = {
-    title:{
-        text:'React.js小书',
-        color:'red'
-    },
-    content:{
-        text: 'React.js 小书内容',
-        color: 'blue'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
+import Header from './Header'
+import Content from './Content'
+import './index.css'
+
+class Index extends Component {
+    render () {
+        return (
+            <div>
+                <Header />
+                <Content />
+            </div>
+        )
     }
 }
 
-function dispatch(action) {
-    switch (action.type){
-        case 'UPDATE_TITLE_TEXT':
-            appState.title.text = action.text
-            break;
-        case 'UPDATE_TITLE_COLOR':
-            appState.title.color = action.color
-            break;
-        default:
-            break
-    }
-}
-
-function renderApp(appState) {
-    renderTitle(appState.title)
-    renderContent(appState.content)
-}
-
-function renderTitle(title) {
-    const titleDom = document.getElementById('title')
-    titleDom.innerHTML = title.text
-    titleDom.style.color = title.color
-}
-
-function renderContent(content) {
-    const contentDom = document.getElementById('content')
-    contentDom.innerHTML = content.text
-    contentDom.style.color = content.color
-}
-renderApp(appState) // 首次渲染页面
-dispatch({
-    type:'UPDATE_TITLE_TEXT',
-    text:'黄澈'
-})
-dispatch({
-    type:'UPDATE_TITLE_COLOR',
-    color:'yellow'
-})
-renderApp(appState) // 把新的数据渲染到页面上
+ReactDOM.render(
+    <Index />,
+    document.getElementById('root')
+)
