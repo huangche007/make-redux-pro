163244603504be43a862b22efb0260419489f941
huangche <hc386271623@163.com>
2019-07-08 18:56:22 +0800

    init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# 手写实现一个Redux
+> 核心点：state\store\reducer\container
---
+++ src/CommentApp.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+class CommentApp extends Component{
+
+    render(){
+        return(
+            <div>
+               111
+            </div>
+        )
+    }
+}
+
+export default CommentApp
