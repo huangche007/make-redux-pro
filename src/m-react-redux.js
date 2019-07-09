b2d273609ee83f1df2117be925a5f9da48c4743e
huangche <hc386271623@163.com>
2019-07-09 13:22:29 +0800

    完善redux

--- src/m-react-redux.js
+++ src/m-react-redux.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react'
 import PropTypes from 'prop-types'
-export const connect = (mapStateToProps)=> (WrappedComponent) => {
+export const connect = (mapStateToProps,mapDispatchToProps)=> (WrappedComponent) => {
     class Connect extends Component{
         static contextTypes= {
             store:PropTypes.object
@@ -19,11 +19,25 @@ export const connect = (mapStateToProps)=> (WrappedComponent) => {
             })
         }
         _updateProps(){
-            const { store } = this.context
+           /* const { store } = this.context
             let stateProps = mapStateToProps(store.getState(), this.props) // 额外传入 props，让获取数据更加灵活方便
             this.setState({
                 allProps: { // 整合普通的 props 和从 state 生成的 props
                     ...stateProps,
+                    ...this.props
+                }
+            })*/
+            const { store } = this.context
+            let stateProps = mapStateToProps
+                ? mapStateToProps(store.getState(), this.props)
+                : {} // 防止 mapStateToProps 没有传入
+            let dispatchProps = mapDispatchToProps
+                ? mapDispatchToProps(store.dispatch, this.props)
+                : {} // 防止 mapDispatchToProps 没有传入
+            this.setState({
+                allProps: {
+                    ...stateProps,
+                    ...dispatchProps,
                     ...this.props
                 }
             })
@@ -35,5 +49,33 @@ export const connect = (mapStateToProps)=> (WrappedComponent) => {
         }
     }
     return Connect
+}
+
+/**
+ * Provider 做的事情也很简单，它就是一个容器组件，会把嵌套的内容原封不动作为自己的子组件渲染出来。
+ * 它还会把外界传给它的 props.store 放到 context，这样子组件 connect 的时候都可以获取到。
+ */
+export class Provider extends Component{
+    static propTypes = {
+        store:PropTypes.object,
+        children:PropTypes.any
+    }
+
+    static childContextTypes = {
+        store:PropTypes.object
+    }
+
+    getChildContext(){
+        return {
+            store:this.props.store
+        }
+    }
+
+    render(){
+        return(
+            <div>{this.props.children}</div>
+        )
+    }
+
 }
 
