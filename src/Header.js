c53d72540528bf36ed47180742f5e13c0c01bbe9
huangche <hc386271623@163.com>
2019-07-09 11:01:33 +0800

    将重复逻辑进行抽取到connect(高阶组件)中

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import { connect } from './m-react-redux'
 class Header extends Component {
+    /*static contextTypes = {
+        store:PropTypes.object
+    }
+
+    constructor(){
+        super()
+        this.state = {
+            themeColor:''
+        }
+    }
+    componentWillMount(){
+        const {store} = this.context
+        this._updateThemeColor();
+        store.subscribe(()=>this._updateThemeColor())
+    }
+    _updateThemeColor(){
+        const {store} = this.context;
+        const state = store.getState()
+        console.log('state:',state)
+        this.setState({
+            themeColor:state.themeColor
+        })
+    }*/
+    static propTypes = {
+        themeColor:PropTypes.string
+    }
     render () {
         return (
-            <h1>手写Redux</h1>
+            <h1 style={{color:this.props.themeColor}}>手写Redux Header</h1>
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return{
+        themeColor:state.themeColor
+    }
+}
+
+Header = connect(mapStateToProps)(Header)
 
 export default Header
